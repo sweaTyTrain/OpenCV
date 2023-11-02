0554f3b369dbca2c5f4c4e8331c030013e27a4d1
HoneyWater8 <kjyjyh8@gmail.com>
2023-11-02 15:10:59 +0900

    add 3D HPE
    이미지에 대한 3D관절 인식, 시각화 및 무릎각도 계산

--- 수헌/3DsqatCnt(미완).py
+++ 수헌/3DsqatCnt(미완).py
@@ -5,6 +5,7 @@ import numpy as np
 from time import time
 import mediapipe as mp
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Initializing mediapipe pose class.
 # mediapipe pose class를 초기화 한다.
@@ -41,7 +42,7 @@ def detectPose(image, pose, display=True):
 
     # input image의 너비&높이 탐색
     height, width, _ = image.shape
-
+    print(height, width)
     # detection landmarks를 저장할 빈 list 초기화
     landmarks = []
 
@@ -55,26 +56,28 @@ def detectPose(image, pose, display=True):
       for landmark in results.pose_landmarks.landmark:
 
         # landmark를 list에 추가하기
-        landmarks.append((float(landmark.x * width), float(landmark.y * height), float(landmark.z * width)))
+        # 이미지의 비율에 맞게 값을 곱해 정규화 해제
+        # z값은 이미지의 비율을 알 수 없으므로 대충 너비의 나누기 3한 값을 곱해준다. -> 나중에 적당한 z가중치를 찾는 코드 추가해야 할 듯
+        landmarks.append((float(landmark.x * width), float(landmark.y * height), float(landmark.z * width / 3)))
 
     # 오리지널 image와 pose detect된 image 비교
     if display:
 
-      # 오리지널 & 아웃풋 이미지 그리기
-      plt.figure(figsize=[17,17])
+        # 오리지널 & 아웃풋 이미지 그리기
+        plt.figure(figsize=[17,17])
 
-      plt.subplot(121)
-      plt.imshow(image[:,:,::-1])
-      plt.title("Original Image")
-      plt.axis('off')
+        plt.subplot(121)
+        plt.imshow(image[:,:,::-1])
+        plt.title("Original Image")
+        plt.axis('off')
 
-      plt.subplot(122)
-      plt.imshow(output_image[:,:,::-1])
-      plt.title("Output Image")
-      plt.axis('off')
+        plt.subplot(122)
+        plt.imshow(output_image[:,:,::-1])
+        plt.title("Output Image")
+        plt.axis('off')
 
-      # 3D 랜드마크 나타내기
-      mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+        # 3D 랜드마크 나타내기
+        mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
 
     return results, output_image, landmarks
 
@@ -83,31 +86,33 @@ def detectPose(image, pose, display=True):
 # 3차원 관절 각도 계산 함수
 # Calculate the angle between three 3D landmarks using unit vectors
 def calculateAngle(landmark1, landmark2, landmark3):
-    # Get the required landmarks coordinates.
-    x1, y1, _ = landmark1
-    x2, y2, _ = landmark2
-    x3, y3, _ = landmark3
+    # landmark1에서 landmark2로 향하는 3차원 벡터 계산
+    vector1 = np.array(landmark1) - np.array(landmark2)
+    # landmark3에서 landmark2로 향하는 3차원 벡터 계산
+    vector2 = np.array(landmark3) - np.array(landmark2)
 
-    # Calculate the angle between the three points
-    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+    # 벡터의 크기를 고려하지 않기 위해 단위벡터로 환산
+    unit_vector1 = vector1 / np.linalg.norm(vector1)
+    unit_vector2 = vector2 / np.linalg.norm(vector2)
 
-    # Check if the angle is less than zero.
-    if angle < 0:
-        # Add 360 to the found angle.
-        angle += 360
+    # 두 단위벡터의 내적을 구한다.(내적: 두 벡터의 방향 유사도(-1~1))
+    dot_product = np.dot(unit_vector1, unit_vector2)
 
-    # Return the calculated angle.
-    return angle
+    # 내적을 바탕으로 두 단위벡터 사이의 각을 구한다.(라디안)
+    angle_radians = math.acos(np.clip(dot_product, -1.0, 1.0))
 
+    # 라디안 각을 degree로 변환
+    angle_degrees = math.degrees(angle_radians)
+
+    return angle_degrees
 
 # pose detection function start
 
-'''
 # 이미지 관절인식 결과 확인 코드
 
 # image = cv2.imread('./sqat.jfif') # 긍정적인 예
 # image = cv2.imread('./pose_img3.jfif') # 부정적인 예
-image = cv2.imread('./sqat_img5.PNG')
+image = cv2.imread('./sqat_img6.PNG')
 results, output_image, landmarks = detectPose(image, pose, display=True)
 
 # landmark가 하나라도 인식되었는지 확인
@@ -142,6 +147,37 @@ if results.pose_world_landmarks:
 else:
     print("랜드마크 인식 불가")
     plt.show()
+
+# 3D 서브플롯을 생성합니다.
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = []
+y = []
+z = []
+
+for i in range(33):
+    x.append(landmarks[i][0])
+    y.append(landmarks[i][2])
+    z.append(-1*landmarks[i][1])
+
+# scatter 함수를 사용하여 3차원 점을 그립니다.
+ax.scatter(x, y, z)
+
+# 아래 옵션은 더 나은 시각화를 위해 조정되는 옵션임
+# 만일 모델이 찌그러져 나와도 실제 점은 landmarks에 제대로 찍혀있다.
+# ax.set_box_aspect([20, 20, 20]) # sqat.jfif 비율
+# ax.set_box_aspect([25, 10, 20]) # pose_img3.jfif 비율
+ax.set_box_aspect([50, 25, 100])    # sqat_img6.PNG 비율
+
+# 축 레이블을 설정합니다.
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# 그래프를 표시합니다.
+plt.show()
+
 
 '''
 
@@ -181,8 +217,8 @@ while video.isOpened():
     frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
 
     # 관절 인식 수행
-    results, frame, landmarks = detectPose(frame, pose_video, display=False)
-    print(landmarks)
+    results, frame, landmarks = detectPose(frame, pose_video, display=True)
+
     # landmark가 하나라도 인식되었는지 확인
     if results.pose_world_landmarks:
         # 최소 신뢰도
@@ -199,14 +235,14 @@ while video.isOpened():
                                               landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value],
                                               landmarks[mp_pose.PoseLandmark.LEFT_HIP.value])
             # 각도를 화면, 콘솔에 출력
-            cv2.putText(frame, f"left_knee_angle: {left_knee_angle}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.6,(255, 0, 0), 2)
+            cv2.putText(frame, f"left_knee_angle: {left_knee_angle}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8,(0, 255, 0), 1)
             print(left_knee_angle)
 
             # 만약 오른쪽 무릎의 각도가 140 이상이면 0(서있는 상태)
-            if left_knee_angle >= 140:
+            if left_knee_angle >= 100:
                 squatNowState = 0
             # 90 이하라면 1(앉은 상태)
-            elif left_knee_angle <= 90:
+            elif left_knee_angle <= 50:
                 squatNowState = 1
 
             # 만약 squatBeforeState(이전 상태)가 1(앉은 상태)였는데
@@ -226,7 +262,7 @@ while video.isOpened():
         cv2.putText(frame, "all landmarks not detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
         print("랜드마크 검출 불가")
 
-    #cv2.putText(frame, f"squatCnt: {squatCnt}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+    cv2.putText(frame, f"squatCnt: {squatCnt}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
     print("스쿼트 개수:", squatCnt)
 
     # 프레임 출력
@@ -246,3 +282,4 @@ video.release()
 
 # Close the windows.
 cv2.destroyAllWindows()
+'''
