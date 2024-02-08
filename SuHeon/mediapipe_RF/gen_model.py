6161cddcd7b4fc137dec24dfd1e6b1f36959b386
HoneyWater8 <101919021+HoneyWater8@users.noreply.github.com>
2024-02-08 16:39:04 +0900

    lgbm evaluate
    lgbm 평가 완

--- SuHeon/mediapipe_RF/gen_model.py
+++ SuHeon/mediapipe_RF/gen_model.py
@@ -24,6 +24,6 @@ gen_model_module.RF_2D_angle(train_route, model_save_route, test_base_route, Fal
 # # RandomForest, 3D, point
 # train_route = '../data/train/3D_point_csv/3D_point.csv'
 # test_base_route = '../data/test/3D_point_csv/'
-# model_save_route = '../models/rf_model_3D_point_test.joblib'
+# model_save_route = '../models/rf_model_3D_point.joblib'
 #
 # gen_model_module.RF_3D_point(train_route, model_save_route, test_base_route, False)
