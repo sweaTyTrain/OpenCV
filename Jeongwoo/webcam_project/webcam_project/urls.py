05cd5b915eee764b379205bccf954968c5da6d43
gstls <105614098+gstls@users.noreply.github.com>
2024-02-14 01:43:57 +0900

    Delete Jeongwoo/webcam_project directory

--- Jeongwoo/webcam_project/webcam_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from webcam_app.views import index, webcam_with_pose
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('webcam_with_pose/', webcam_with_pose, name='webcam_with_pose'),
-]
